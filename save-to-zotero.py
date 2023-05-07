6131e9d850a53d2c02307d201a321faed609bdc0
YI <uuuuuu@protonmail.com>
2023-05-07 13:43:10 +0800

    use firefox to save to zotero

--- save-to-zotero.py
+++ save-to-zotero.py
@@ -1,30 +1,31 @@
 import os
 
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.firefox.service import Service
 
 
 def main():
     print(f"starting")
     options = Options()
-    options.add_argument("--no-sandbox")
-    options.add_extension(
-        os.path.join(os.path.dirname(__file__), "connector/extension_5_0_108_0.crx")
-    )
+    # options.add_argument('--headless')
     service = Service()
-    service.executable_path = "chromium"
-    driver = webdriver.Chrome(service=service, options=options)
+    driver = webdriver.Firefox(service=service, options=options)
     print(f"obtained webdriver")
 
-    # zotero_connector = os.path.join(
-    #     os.path.dirname(__file__), "connector/extension_5_0_108_0.crx"
-    # )
-    # print(f"installing {zotero_connector}")
-    # driver.install_addon(zotero_connector)
-    # print(f"installing {zotero_connector}")
+    zotero_connector = os.path.join(
+        os.path.dirname(__file__), "connector/Zotero_Connector-5.0.108.xpi"
+    )
+    print(f"installing {zotero_connector}")
+    driver.install_addon(zotero_connector)
+    driver.install_addon(
+        os.path.join(
+            os.path.dirname(__file__), "connector/uBlock0_1.49.2.firefox.signed.xpi"
+        )
+    )
+    print(f"installing {zotero_connector}")
 
-    url = "moz-extension://eae1d601-094c-486e-b8d1-edd8e0a7bfdb/"
     url = "http://baidu.com"
     print(f"opening {url}")
     driver.get(url)
